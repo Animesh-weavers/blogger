78bad7958a0cb2bcd1f9b0bc0f9f194aa7fabb05
Animesh-weavers <animesh.dey@weavers-web.com>
2022-05-16 11:25:44 +0530

    db connectivity

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.use("/api", (req, res, next) => {
-  res.send("Hello world");
-});
-
-app.listen(5000, () => {
-  console.log("Server is running on port: 5000");
-});
+mongoose
+  .connect(
+    "mongodb+srv://animesh2022:animeshdey1234@cluster0.e7jce.mongodb.net/blog?retryWrites=true&w=majority"
+  )
+  .then(() => app.listen(5000))
+  .then(() =>
+    console.log("Connected to database and listening to localhost: 5000!!")
+  )
+  .catch((err) => {
+    console.log(err);
+  });
